7c39e87b0b13d777cbb05f148e5c0efc7e2544e1
clement <clementpaquentin@hotmail.fr>
2025-05-03 02:13:57 +0200

    feat(assets): add forum content

---
+++ pages/forum/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello forum</h1>
+  <article v-for="post in posts" :key="post.id">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body.slice(0, 1000) }}</p>
+    <p>
+      <NuxtLink :to="`/forum/${post.id}`">Lire la suite</NuxtLink>
+    </p>
+  </article>
+</template>
+
+<script setup>
+const {data: posts} = await useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+</script>
