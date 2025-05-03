3cad2980a2471c6145ef35aa02460998d85aa01e
clement <clementpaquentin@hotmail.fr>
2025-05-03 12:21:09 +0200

    chore(config): add nuxtUI

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <UApp>
     <NuxtLoadingIndicator/>
     <NuxtRouteAnnouncer/>
     <NuxtPage/>
-  </div>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image',
-    '@vesp/nuxt-fontawesome'
-  ],
-  css: [
-    'public/assets/css/styles.css',
-  ],
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/ui',
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image',
+        '@vesp/nuxt-fontawesome',
+    ],
+    css: [
+        'public/assets/css/styles.css',
+    ],
 })
