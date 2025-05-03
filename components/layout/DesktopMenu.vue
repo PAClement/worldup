b6c13a86e7843ba1c879b670d606e94e18dcf460
clement <clementpaquentin@hotmail.fr>
2025-05-04 01:19:04 +0200

    fix(component): update links in DesktopMenu for correct routing

--- components/layout/DesktopMenu.vue
+++ components/layout/DesktopMenu.vue
@@ -8,16 +8,15 @@ const classes = {
 <template>
   <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="hidden lg:flex lg:items-center lg:gap-x-12">
-      <NuxtLink to="/public">
+      <NuxtLink to="/">
         <NuxtImg
           src="/assets/img/logo.png"
           alt="Logo"
           class="h-12 w-auto"/>
       </NuxtLink>
-      <NuxtLink to="/public" :class="classes.default" :exact-active-class="classes.active">Qui sommes-nous ?</NuxtLink>
       <NuxtLink to="/news" :class="classes.default" :exact-active-class="classes.active">News</NuxtLink>
       <NuxtLink to="/forum" :class="classes.default" :exact-active-class="classes.active">Forum</NuxtLink>
-      <NuxtLink to="/public" :class="classes.default" :exact-active-class="classes.active">Map</NuxtLink>
+      <NuxtLink to="/map" :class="classes.default" :exact-active-class="classes.active">Map</NuxtLink>
     </div>
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
       <NuxtLink to="/auth/login" :class="classes.default" :exact-active-class="classes.active">
