108928fa1ad5b934403c76e53d9e9c7d51e20772
clement <clementpaquentin@hotmail.fr>
2025-05-03 02:15:11 +0200

    feat(page): add vue

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Rajdhani:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+  <div class="bg-[url(/assets/img/main_bg.jpg)] h-dvh">
+    <div class="bg-black/50 backdrop-invert backdrop-opacity-10 h-dvh">
+      <Header :transparent="true"/>
+      <div class="flex flex-col justify-between items-center">
+        <div>
+          <NuxtImg
+              src="/assets/img/logo_full.png"
+              alt="Logo"
+              class="h-72 w-auto mx-auto mt-10"/>
+          <h1 class="text-6xl text-center text-accent-100 my-10 font-bebas">Commencer votre <br> <span
+              class="text-primary-500">aventure !</span></h1>
+        </div>
+        <font-awesome :icon="faArrowDown" class="text-4xl text-primary-500 motion-safe:animate-bounce"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
+
+</script>
