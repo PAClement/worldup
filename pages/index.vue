d09b4dcf73aa9f86f1bf4eb8c38cf4b7ba3b4a1e
clement <clementpaquentin@hotmail.fr>
2025-05-04 00:54:22 +0200

    feat(pages): add Header component and update layout structure

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
           class="h-72 w-auto"/>
       <div class="pl-5">
         <h2>Hello world</h2>
-        <p>
+        <p class="custom-body">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
           commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
