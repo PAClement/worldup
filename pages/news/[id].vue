cc3d368d9c18e3e2302d24608b91905d4eb36f7b
clement <clementpaquentin@hotmail.fr>
2025-05-03 11:48:46 +0200

    feat(pages): add news page

---
+++ pages/news/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <Header :transparent="false"/>
+  <p>la news</p>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Header :transparent="false"/>
+  <p>les news</p>
+</template>
+
+<script setup lang="ts">
+
+</script>
