1c02e02954f71117a6ceef70972be1ce388a67d2
Alexis <azen0x.alexis@gmail.com>
2025-05-03 15:35:11 +0200

    :bug: fix tailwind versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     modules: [
         '@nuxt/ui',
-        '@nuxtjs/tailwindcss',
         '@nuxt/image',
         '@vesp/nuxt-fontawesome',
     ],
     css: [
         'public/assets/css/styles.css',
     ],
+    vite: {
+        plugins: [tailwindcss()],
+    }
 })
